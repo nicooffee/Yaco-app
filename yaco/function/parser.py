62225648d26bf75377a5043f1fe62760ca53b319
matiashc92 <matias.hurtado@usach.cl>
2020-06-14 20:22:57 -0400

    arreglado parsing, cuando las palabras carecen de link. Ademas de regularizar el output para los casos de input encontrados hasta ahora.

--- yaco/function/parser.py
+++ yaco/function/parser.py
@@ -19,9 +19,23 @@ def def_parser(D):
         start = word+1
     for word in wordList:
         try:
-            wordForm.append(re.search('{a_link\|(.+?)$', word).group(1).split("}"))
+            regex = re.search('{a_link\|(.+?)$', word).group(1).split('}')
+            if len(regex[1]) > 2 and regex[1][0] == ' ':
+                wordForm.append([regex[0], regex[1][1:]])
+            else:
+                wordForm.append(regex)
         except AttributeError:
-            return None
+            try:
+                regex = re.search('}([^{}]+?)$', word).group(1).split('(')
+                if len(regex) == 1:
+                    wordForm.append([regex[0], ''])
+                else:
+                    wordForm.append([regex[0], '('+'('.join(regex[1:])])
+            except AttributeError:
+                wordForm.append(None)
+    for word in wordForm:
+        if re.fullmatch(' +',word[1]):
+            word[1] = ''
     return wordForm
 
 if __name__ == "__main__":

2f23c18719e6cfc4c9487c1b12fe6bb6e4de8f17
nicooffee <nicolas.honorato@usach.cl>
2020-07-29 21:29:08 -0400

    a

--- yaco/app.py
+++ yaco/app.py
@@ -7,4 +7,5 @@ app.register_blueprint(principal)
 
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)

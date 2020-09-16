7be87b4c74a13770d76350fa26a65ebfb3b08593
nicooffee <nicolas.honorato@usach.cl>
2020-09-16 02:05:33 -0300

    cambiado database data

--- yaco/DatabaseData.py
+++ yaco/DatabaseData.py
@@ -1,11 +1,8 @@
-user = "nico@yacoserver"
-host = "yacoserver.postgres.database.azure.com"
+user = "nico"
+host = "127.0.0.1"
 port = 5432
 db_model_name = "yaco" 
-db_model_pswd = "Yaco1234"
+db_model_pswd = "postgres"
 db_sess_name = "sessions"
-db_sess_pswd = "Yaco1234"
-
-
-
+db_sess_pswd = "postgres"
 

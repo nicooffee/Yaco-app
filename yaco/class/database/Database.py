263555574abe6a05bffed40d4512655f3a24444a
nicooffee <nicolas.honorato@usach.cl>
2020-09-19 14:31:12 -0300

    arreglo a conexión (reactivar si está desactivada)

--- yaco/class/database/Database.py
+++ yaco/class/database/Database.py
@@ -2,48 +2,68 @@ import psycopg2
 import DatabaseData as dbdata
 class PSConnection:
     def __init__(self):
-        self.connection = psycopg2.connect(
-            user=dbdata.user,
-            password=dbdata.db_model_pswd,
-            host=dbdata.host,
-            port=dbdata.port,
-            database=dbdata.db_model_name)
+        self.connection = self.__create_conn()
         self.cursor = self.connection.cursor()
 
     def query(self,query,params):
         try:
             res = self.cursor.execute(query,params)
             self.connection.commit()
-            return self.cursor.rowcount
         except (Exception, psycopg2.Error) as error :
             print("Error al ejecutar query -- ", error)
-            return -1
+            self.connection = self.__create_conn()
+            self.cursor = self.connection.cursor()
+            res = self.cursor.execute(query,params)
+            self.connection.commit()
+        finally:
+            return self.cursor.rowcount
+
+
     def query_many(self,query,p_list):
         try:
             res = self.cursor.executemany(query,p_list)
             self.connection.commit()
-            return self.cursor.rowcount
         except (Exception, psycopg2.Error) as error :
             print("Error al ejecutar query -- ", error)
-            return -1
-
+            self.connection = self.__create_conn()
+            self.cursor = self.connection.cursor()
+            res = self.cursor.executemany(query,p_list)
+            self.connection.commit()
+        finally:
+            return self.cursor.rowcount
 
     def fetch_one(self,query,p_list):
         try:
             res = self.cursor.execute(query,p_list)
             self.connection.commit()
-            return self.cursor.fetchone()
         except (Exception, psycopg2.Error) as error :
             print("Error al ejecutar query -- ", error)
-            return -1
+            self.connection = self.__create_conn()
+            self.cursor = self.connection.cursor()
+            res = self.cursor.execute(query,p_list)
+            self.connection.commit()
+        finally:  
+            return self.cursor.fetchone()
     def fetch_all(self,query,p_list):
         try:
             res = self.cursor.execute(query,p_list)
             self.connection.commit()
-            return self.cursor.fetchall()
         except (Exception, psycopg2.Error) as error :
             print("Error al ejecutar query -- ", error)
-            return -1
+            self.connection = self.__create_conn()
+            self.cursor = self.connection.cursor()
+            res = self.cursor.execute(query,p_list)
+            self.connection.commit()
+        finally:
+            return self.cursor.fetchall()
+
+    def __create_conn():
+        return psycopg2.connect(
+            user=dbdata.user,
+            password=dbdata.db_model_pswd,
+            host=dbdata.host,
+            port=dbdata.port,
+            database=dbdata.db_model_name)
 
     def __del__(self):
         if(self.connection):
